b6b491f89673bc4cb7129d9ab23581abe5990850
Camilla Knutsen <camilla.knutsen89@gmail.com>
2016-10-14 09:34:07 +0200

    Add files via upload
    Quick fix, added alfa changeWidth() function

--- MillusHtml/js/script.js
+++ MillusHtml/js/script.js
@@ -7,11 +7,20 @@
     for (var i = 0; i <= numOfBoxElements; i += 1) {
       boxElements[i].onclick = function(e) {
         var visible_choice = document.getElementById('visible-choice').innerHTML;
+		var boxChosen = document.getElementById('visible-choice').id;
         var boxClicked = this.id;
+		changeWidth(boxChosen,boxClicked);
         var temp = document.getElementById(boxClicked).innerHTML;
         document.getElementById(boxClicked).innerHTML = visible_choice;
         document.getElementById('visible-choice').innerHTML = temp;
     };
 }
+
+function changeWidth(idChosen, idClicked){
+	var newValue = 0;
+	var value = document.getElementById(idChosen).innerHTML;
+	newValue = parseInt(value) * 10;
+	document.getElementById(idClicked).style.width = newValue + 'px'; 
+};
 
 })();
